3f0c54b09a79b83367f9ae82322c138639f59f46
A.Jelouani <jelouaniabdelghani@gmail.com>
2024-02-27 11:03:19 +0100

    git commit -m "FR-10 enums Created"

---
+++ src/main/java/com/youcode/tournoi/enums/TypeEquipe.java
@@ -0,0 +1,6 @@
+package com.youcode.tournoi.enums;
+
+public enum TypeEquipe {
+    A1,
+    A2
+}
---
+++ src/main/java/com/youcode/tournoi/enums/TypeTournoi.java
@@ -0,0 +1,8 @@
+package com.youcode.tournoi.enums;
+
+public enum TypeTournoi {
+    TootBall,
+    Fifa,
+    BabyFoot,
+    BasketBall,
+}
